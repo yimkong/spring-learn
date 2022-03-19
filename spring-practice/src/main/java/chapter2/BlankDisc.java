50649716783dd099fc9eb7002240f6fe56671df2
yimkong <351072414@qq.com>
2022-03-19 23:10:08 +0800

    p schema space

--- spring-practice/src/main/java/chapter2/BlankDisc.java
+++ spring-practice/src/main/java/chapter2/BlankDisc.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.util.List;
+
 /**
  * 2022/3/14
  * desc
@@ -7,12 +9,30 @@ package chapter2;
 public class BlankDisc implements CompactDisc{
     private String title;
     private String artist;
-    public BlankDisc(String title,String artist){
+    private List<String> tracks;
+
+//    public BlankDisc(String title,String artist){
+//        this.title = title;
+//        this.artist = artist;
+//    }
+
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setArtist(String artist) {
         this.artist = artist;
     }
+
+    public void setTracks(List<String> tracks) {
+        this.tracks = tracks;
+    }
+
     @Override
     public void play() {
         System.out.println("playing" + title + " by " + artist);
+        for (String track : tracks) {
+            System.out.println("-Track: "+ track);
+        }
     }
 }
---
+++ spring-practice/src/test/java/BlankDiscTest.java
@@ -0,0 +1,26 @@
+import chapter2.BlankDisc;
+import chapter2.CDPlayerConfig;
+import chapter2.CompactDisc;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+/**
+ * 2022/3/19
+ * desc
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:application.xml")
+public class BlankDiscTest {
+    @Autowired
+    BlankDisc blankDisc;
+
+    @Test
+    public void test() {
+        Assert.notNull(blankDisc,"oh");
+        blankDisc.play();
+    }
+}
